21dfa25be7b75038cfe5084074bf50936aac1538
tomiho19 <sandrez.spartanell@gmail.com>
2018-03-04 18:37:18 +0200

    handlebars

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-// let express = require('express');
-//
-// let app = express();
-//
-// app.set('port', process.env.PORT || 3000)
-function sayHi() {
-    return ()=> console.log('hello');
-}
+let express = require('express');
+let app = express();
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
-let say = sayHi();
-say();
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), ()=>{
+    console.log('Server\'s running on http://localhost:' + app.get('port') + ' press Ctrl + C to exit');
+});
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+//////////////////////////////////////////////////////////////
+let fortunes = [
+    "Победи свои страхи, или они победят тебя.",
+    "Рекам нужны истоки.",
+    "Не бойся неведомого.",
+    "Тебя ждет приятный сюрприз.",
+    "Будь проще везде, где только можно.",
+];
+//////////////////////////////////////////////////////////////
+app.get('/about', (req, res)=>{
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
+
+app.use((req, res, next)=>{
+   res.status(404);
+   res.render('404');
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
